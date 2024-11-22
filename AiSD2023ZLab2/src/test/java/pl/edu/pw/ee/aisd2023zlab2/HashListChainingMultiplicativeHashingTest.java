b782be54f7f543f38054babd9d3f269a8f21de53
xiopzs <jjszopa@o2.pl>
2024-11-22 16:24:30 +0100

    naprawa

--- AiSD2023ZLab2/src/test/java/pl/edu/pw/ee/aisd2023zlab2/HashListChainingMultiplicativeHashingTest.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.ee.aisd2023zlab2;
-
-import pl.edu.pw.ee.aisd2023zlab2.utils.GeneralHashListChainingTest;
-
-public class HashListChainingMultiplicativeHashingTest extends GeneralHashListChainingTest {
-    public HashListChainingMultiplicativeHashingTest() {
-        super(HashListChainingModularHashing.class);
-    }
-}
--- AiSD2023ZLab3/src/main/java/pl/edu/pw/ee/aisd2023zlab3/exceptions/NotImplementedException.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.ee.aisd2023zlab3.exceptions;
-
-public class NotImplementedException extends RuntimeException {
-
-    public NotImplementedException(String message) {
-        super(message);
-    }
-
-}
